1aa6334d549e01ec588ca7b3de572ccbc440b720
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-09-30 14:28:25 +0530

    added theme switcher

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardPage() {
-  return <div></div>;
+  return (
+    <div className="flex-end">
+      <ModeToggle />
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import { ThemeProvider } from "@/providers/theme-provider";
+
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      storageKey="cavic-theme"
+    >
+      <div className="h-full relative">
+        <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md-inset-y-0 z-[80] dark:bg-[#2B2D31] bg-[#F2F3F5]">
+          Sidebar
+        </div>
+        <main className="h-full md:pl-72 bg-white dark:bg-zinc-900">
+          {children}
+        </main>
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default DashboardLayout;
