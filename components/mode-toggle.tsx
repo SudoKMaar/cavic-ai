a0c19e3ce31ef3e2d55e6c22f899dabec866c22f
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-09-30 16:19:26 +0530

    added dashboard

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,13 +18,17 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="bg-transparent">
+        <Button
+          variant="outline"
+          size="icon"
+          className="bg-white dark:bg-zinc-900"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="bg-transparent">
+      <DropdownMenuContent align="end" className="bg-white dark:bg-zinc-900">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
